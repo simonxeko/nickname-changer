f812c97431fbc2271c0ac36849fa36ca0dbc89d6
Simon Ko <simonxeko@gmail.com>
2020-01-14 10:35:33 +0800

    Update migration again

--- migrations/2019_07_28_000000_add_nickname_column.php
+++ migrations/2019_07_28_000000_add_nickname_column.php
@@ -3,9 +3,10 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
+
 return [
     'up' => function (Builder $schema) {
-        if (!Schema::hasColumn('users', 'nickname')) {
+        if (!$schema->hasColumn('users', 'nickname')) {
             $schema->table('users', function (Blueprint $table) {
                 $table->string('nickname', 30)->after('username');
             });
